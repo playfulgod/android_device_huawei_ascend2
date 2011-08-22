5a6a5e784862f591d263022327c2df8e82c7af82
Dewayne <playfulgod@gmail.com>
2011-08-21 21:24:09 -0300

    Config for CWM recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/huawei/ascend2/recovery/recover
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_PREBUILT_RECOVERY_KERNEL := device/huawei/ascend2/recovery_kernel
 
+# fix this up by examining /proc/mtd on a running device
 ## cat /proc/mtd
 # dev:    size   erasesize  name
 # mtd0: 00500000 00020000 "boot"
@@ -33,51 +34,15 @@ TARGET_PREBUILT_RECOVERY_KERNEL := device/huawei/ascend2/recovery_kernel
 # mtd7: 01400000 00020000 "userdata2"
 # mtd8: 01400000 00020000 "cust"
 
-# fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00500000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0ba00000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0a0a0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Wifi related defines
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-#WPA_SUPPLICANT_VERSION := VER_0_6_X
-#WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcm4319.ko"
-#WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/firmware/fw_bcm4319.bin nvram_path=/system/etc/firmware/nvram.txt"
-#WIFI_DRIVER_MODULE_NAME := "dhd"
-
-# Graphics
-TARGET_HARDWARE_3D := false
-TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
-BOARD_NO_RGBX_8888 := true
-BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
-BOARD_HAS_LIMITED_EGL := true
-
-# Audio
-TARGET_PROVIDES_LIBAUDIO := true
-BOARD_USES_GENERIC_AUDIO := false
-
-# HW
-TARGET_USES_OLD_LIBSENSORS_HAL := true
-BOARD_HAVE_BLUETOOTH := true
-BOARD_USES_QCOM_HARDWARE := true
-BOARD_USES_QCOM_LIBS := true
-BOARD_USES_QCOM_LIBRPC := true
-BOARD_VENDOR_USE_AKMD := akm8975
-
-# GPS
-BOARD_USES_QCOM_GPS := true
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := M865
-#BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 6225
-
 # USB
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
-
-TARGET_OTA_ASSERT_DEVICE := ascend2,M865
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_BOOTLOADER_BOARD_NAME=M865
-TARGET_BOOTLOADER_BOARD_NAME := M865
 
 TARGET_PREBUILT_KERNEL := device/huawei/ascend2/kernel
 
