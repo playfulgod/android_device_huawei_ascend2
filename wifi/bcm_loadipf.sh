508132c2b709b2e3376d03bce1b4c3e0208361d6
playfulgod <playfulgod@gmail.com>
2011-09-30 00:10:33 -0400

    deleted:    .directory 	deleted:    AudioFilter.csv 	deleted:    AutoVolumeControl.txt 	deleted:    apns-conf.xml 	deleted:    etc/AudioFilter.csv 	deleted:    etc/apns-conf.xml 	deleted:    etc/bluetooth/BCM4329.hcd 	deleted:    etc/bluetooth/audio.conf 	deleted:    etc/bluetooth/auto_pairing.conf 	deleted:    etc/bluetooth/blacklist.conf 	deleted:    etc/bluetooth/init.bcm.bt.sh 	deleted:    etc/bluetooth/init.bcm.chip_off.sh 	deleted:    etc/bluetooth/init.bcm.chip_on.sh 	deleted:    etc/bluetooth/input.conf 	deleted:    etc/bluetooth/main.conf 	deleted:    etc/init.goldfish.sh 	deleted:    etc/init.qcom.bt.sh 	deleted:    etc/init.qcom.coex.sh 	deleted:    etc/init.qcom.fm.sh 	deleted:    etc/init.qcom.post_boot.sh 	deleted:    etc/init.qcom.sdio.sh 	deleted:    etc/vold.fstab 	deleted:    etc/wpa_supplicant.conf 	deleted:    include/app/Launcher2.apk 	deleted:    include/apps2sd_files/apps2sd_run_program 	deleted:    include/apps2sd_files/apps2sd_system 	deleted:    include/apps2sd_files/data/dtinstall.launch 	deleted:    include/apps2sd_files/data/jit/bin/dalvikvm 	deleted:    include/apps2sd_files/data/jit/lib/libdvm.so 	deleted:    include/apps2sd_files/data/jit/lib/libnativehelper.so 	deleted:    include/apps2sd_files/move_cache_sd 	deleted:    include/apps2sd_files/system/bin/a2sd 	deleted:    include/apps2sd_files/system/bin/apps2sd.hlp 	deleted:    include/apps2sd_files/system/bin/bash 	deleted:    include/apps2sd_files/system/bin/busybox.a2sd 	deleted:    include/apps2sd_files/system/bin/chka2sd 	deleted:    include/apps2sd_files/system/bin/dtinstall 	deleted:    include/apps2sd_files/system/bin/jita2sd 	deleted:    include/apps2sd_files/system/bin/launcha2sd 	deleted:    include/apps2sd_files/system/bin/starta2sd 	deleted:    include/apps2sd_files/system/bin/sysinit 	deleted:    include/apps2sd_files/system/bin/zipalign 	deleted:    include/apps2sd_files/system/etc/init.d/00banner 	deleted:    include/apps2sd_files/system/etc/init.d/01sysctl 	deleted:    include/apps2sd_files/system/etc/init.d/02firstboot 	deleted:    include/apps2sd_files/system/etc/init.d/04apps2sd 	deleted:    include/apps2sd_files/system/etc/init.d/99complete 	deleted:    include/apps2sd_files/system/sd/placeholder 	deleted:    include/apps2sd_files/system/xbin/a2sd 	deleted:    include/apps2sd_files/system/xbin/apps2sd 	deleted:    include/apps2sd_files/version.txt 	deleted:    include/bin/vold 	deleted:    include/etc/dhcpcd/dhcpcd-hooks/01-test 	deleted:    include/etc/dhcpcd/dhcpcd-hooks/20-dns.conf 	deleted:    include/etc/dhcpcd/dhcpcd-hooks/95-configured 	deleted:    include/etc/dhcpcd/dhcpcd-run-hooks 	deleted:    include/etc/dhcpcd/dhcpcd.conf 	deleted:    include/etc/firmware/yamato_pfp.fw 	deleted:    include/etc/firmware/yamato_pm4.fw 	deleted:    include/etc/gps.conf 	deleted:    include/etc/media_profiles.xml 	deleted:    include/etc/wifi/wpa_supplicant.conf 	deleted:    init 	deleted:    init.qcom.bt.sh 	deleted:    init.rc 	deleted:    keychars/qwerty.kcm.bin 	deleted:    keychars/qwerty2.kcm.bin 	deleted:    keychars/qwerty_horizontal.kcm.bin 	deleted:    keychars/surf_keypad.kcm.bin 	deleted:    keylayout/7k_handset.kl 	deleted:    keylayout/AVRCP.kl 	deleted:    keylayout/qwerty.kl 	deleted:    keylayout/surf_keypad.kl 	deleted:    libaudio/AudioHardware.cpp~ 	deleted:    media_profiles.xml 	deleted:    modules/dhd.ko 	deleted:    modules/evbug.ko 	deleted:    modules/librasdioif.ko 	deleted:    ueventd.rc 	deleted:    wifi/bcm_loadecho.sh 	deleted:    wifi/bcm_loadipf.sh 	deleted:    wifi/connectap.sh 	deleted:    wifi/iwconfig 	deleted:    wifi/iwlist 	deleted:    wifi/iwpriv 	deleted:    wifi/nvram.txt 	deleted:    wifi/tet.txt 	deleted:    wifi/udp_server 	deleted:    wifi/wifi.mk 	deleted:    wpa_supplicant.conf

--- wifi/bcm_loadipf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-insmod /wifi/dhd.ko "firmware_path=/system/wifi/firmware.bin nvram_path=/system/wifi/nvram.txt"
-sleep 5
--- wifi/connectap.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-cd /system/wifi
-ifconfig eth0 $1 up
-wl join "$2"
-sleep 3
